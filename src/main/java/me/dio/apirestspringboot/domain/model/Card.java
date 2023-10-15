0417bc2ac05b867f94c63a521477cd08a01763a4
Vinicius Santos <vinisantoosk@gmail.com>
2023-10-14 23:47:29 -0300

    fix: precision and scale

--- src/main/java/me/dio/apirestspringboot/domain/model/Card.java
+++ src/main/java/me/dio/apirestspringboot/domain/model/Card.java
@@ -11,7 +11,7 @@ public class Card {
     private Long id;
     @Column(unique = true)
     private String number;
-    @Column(name = "avaible_limit",scale = 13, precision = 2)
+    @Column(name = "avaible_limit",scale = 2, precision = 13)
     private BigDecimal limit;
 
     public Long getId() {
